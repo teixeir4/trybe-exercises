5d6fc478b1ba7432dc52824c3bee707ad92dbad8
Teixeira <contatorhaj@gmail.com>
2022-05-12 18:07:50 -0300

    Exercicio dia 4.1 Operadores Lógicos

--- 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/scripts.js
+++ 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/scripts.js
@@ -1,11 +1,20 @@
-const nota = 78;
+const currentHour = 23;
+let message = "";
 
-if (nota >= 80) {
-    console.log("Parabéns, você foi aprovado(a)")
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
 }
-else if (nota <= 80 && nota >= 60) {
-    console.log("Você está na nossa lista de espera")
+else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
 }
-else {
-    console.log ("Você foi reprovada(o)")
+else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
 }
+else if (currentHour >= 11 && currentHour < 14) {
+  message = "Hora do almoço";
+}
+else if (currentHour >= 4 && currentHour < 11) {
+  message = "HMMM, cheiro de café recém passado"
+}  
+
+console.log(message);
