8f76e93d782abd5030affb6caa9461eacde3fc6e
Rhaj Teixeira <contatorhaj@gmail.com>
2022-05-13 10:48:17 -0300

    Exercício 2 dia 4.1

--- 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/scripts.js
+++ 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/scripts.js
@@ -1,8 +1,9 @@
-const a = 14;
-const b = 7;
+const a = 19;
+const b = 12;
 
-console.log('Soma: ' + (a + b));
-console.log('Subtração: ' + (a - b));
-console.log('Multiplicação: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
+if {a > b} {
+  console.log("a é maior que b");
+}
+else {
+  console.log("b é maior que a");
+};
