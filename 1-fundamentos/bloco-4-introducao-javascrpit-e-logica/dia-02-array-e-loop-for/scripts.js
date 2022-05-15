b0ae4f4216922cf7dd656ec8dd36d2fef37667d4
Rhaj Teixeira <contatorhaj@gmail.com>
2022-05-15 12:40:54 -0300

    Requisito 7

--- 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia-02-array-e-loop-for/scripts.js
+++ 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia-02-array-e-loop-for/scripts.js
@@ -3,7 +3,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorNumero = numbers[0];
 
 for (let index = 1; index < numbers.length; index += 1) {
-    if (numbers[index] > maiorNumero) {
+    if (numbers[index] < maiorNumero) {
         maiorNumero = numbers[index];
     }
 }
