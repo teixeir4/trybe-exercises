9d10c04fbad96d2239036a0ff972673088cf4588
Rhaj Teixeira <contatorhaj@gmail.com>
2022-05-15 12:40:42 -0300

    requisito 7

--- 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia-02-array-e-loop-for/scripts.js
+++ 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia-02-array-e-loop-for/scripts.js
@@ -1,14 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  sum += numbers[index];
-} 
-let media = sum / numbers.length;
+let maiorNumero = numbers[0];
 
-if (media > 20) {
-    console.log("Valor maior que 20.")
-} 
-else {
-    console.log("Valor menor ou igual a 20.")
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    }
 }
+
+console.log(maiorNumero);
