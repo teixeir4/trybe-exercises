29b5d1c881e3cd84089e7dbfe0d3b5bdad60ad33
Rhaj Teixeira <contatorhaj@gmail.com>
2022-05-16 15:39:22 -0300

    Exercicio 1

---
+++ 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia-02-array-e-loop-for/scripts.js
@@ -0,0 +1,8 @@
+// crie um algoritmo que retorne o fatorial de 10.
+
+var fatorial = 10;
+var resultado = fatorial;
+for (var i = 1; i <= fatorial; i += 1) {
+    resultado *= i;
+}
+console.log(resultado);
