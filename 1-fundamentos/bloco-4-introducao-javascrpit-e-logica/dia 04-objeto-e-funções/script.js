567402a5666580c2b67aa1be53783636e7dfec93
Rhaj Teixeira <contatorhaj@gmail.com>
2022-05-17 15:59:11 -0300

    Para Fixar IV

--- 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia 04-objeto-e-funções/script.js
+++ 1-fundamentos/bloco-4-introducao-javascrpit-e-logica/dia 04-objeto-e-funções/script.js
@@ -9,7 +9,7 @@
 
 // Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
-console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' ' + 'tem' + ' ' + player['age'] + ' ' + 'anos de idade.');
+// console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' ' + 'tem' + ' ' + player['age'] + ' ' + 'anos de idade.');
 
 // Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
 
@@ -21,4 +21,6 @@ let player = {
     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
   };
 
-  
+/* Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes". */
+
+console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' ' + 'foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
