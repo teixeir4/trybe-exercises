6430b8581ebec60d40378909f80889b7f9e8d350
Rhaj Teixeira <contatorhaj@gmail.com>
2022-05-31 15:50:55 -0300

    Exercicios

--- 1-fundamentos/bloco-05-javascript-dom-e-seletores/dia-03/script.js
+++ 1-fundamentos/bloco-05-javascript-dom-e-seletores/dia-03/script.js
@@ -1,52 +1,40 @@
-function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  };
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
 
-  /* Exercício 1
-    O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days". Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Segunda-feira e Terça-feira.
-    Os dias devem estar contidos em uma tag <li>, e todos devem ter classe day. Ex: <li class="day">3</li>
-    Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
-    Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li> */
+function insertPhraseInDOM() {
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+}
 
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+function addPhraseToLocalStorage() {
+  const oldList = JSON.parse(localStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  localStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+}
 
-  function createDaysOfTheMonth() {
-      let getDaysList = document.querySelector('#days');
-    
-      for (let i = 0; i < dezDaysList.length; i += 1) {
-          let day = dezDaysList[i];
-          let dayItem = document.createElement('li');
-
-            if(day === 24 | day === 31) {
-                dayItem.className = 'day holiday';
-                dayItem.innerHTML = day;
-                getDaysList.appendChild(dayItem);
-            } else if (day === 4 | day === 11 | day === 18) {
-                dayItem.className = 'day friday';
-                dayItem.innerHTML = day;
-                getDaysList.appendChild(dayItem);
-            } else if (day === 25) {
-                dayItem.className = 'day holiday friday';
-                dayItem.innerHTML = day;
-                getDaysList.appendChild(dayitem);
-            } else {
-                dayItem.className = 'day';
-                dayItem.innerHTML = day;
-                getDaysList.appendChild(dayItem);
-            }      
-        }
+function initialRenderization() {
+  if (localStorage.getItem('phrases') === null) {
+    localStorage.setItem('phrases', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      list.appendChild(listElement);
+    }
   }
-createDaysOfTheMonth();
+}
+
+button.addEventListener('click', addPhraseToLocalStorage);
+
+window.onload = function() {
+  initialRenderization();
+};
