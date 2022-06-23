dc136aabf2d065e49a3749c5702d63c9742479b5
Rhaj Teixeira <contatorhaj@gmail.com>
2022-06-23 13:25:03 -0300

    Exercicio III

--- 1-fundamentos/bloco-8-higher-order-functions/script.js
+++ 1-fundamentos/bloco-8-higher-order-functions/script.js
@@ -14,11 +14,33 @@
 
 //   console.log(newEmployees(aprovedEmployee));
 
-const checarNumero = (meuNumero, numero) => meuNumero === numero;
+// const checarNumero = (meuNumero, numero) => meuNumero === numero;
 
-const resultadoSorteio = (meuNumero, callback) => {
-    const numero = Math.floor((Math.random() * 5) + 1);
+// const resultadoSorteio = (meuNumero, callback) => {
+//     const numero = Math.floor((Math.random() * 5) + 1);
 
-    return callback(meuNumero, numero) ? 'Parabéns você ganhou' : 'Tente novamente';
-}
-console.log(resultadoSorteio(3, checarNumero));
+//     return callback(meuNumero, numero) ? 'Parabéns você ganhou' : 'Tente novamente';
+// }
+// console.log(resultadoSorteio(3, checarNumero));
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado final: ${contador} pontos`;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
