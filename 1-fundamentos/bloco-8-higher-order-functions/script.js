21c1817de124ffb7cf4f0228cfb207f130a740be
Rhaj Teixeira <contatorhaj@gmail.com>
2022-06-23 13:21:01 -0300

    Exercicio II

--- 1-fundamentos/bloco-8-higher-order-functions/script.js
+++ 1-fundamentos/bloco-8-higher-order-functions/script.js
@@ -1,15 +1,24 @@
-const aprovedEmployee = (fullName) => {
-    const email = fullName.toLowerCase().split(' ').join('_');
-    return { fullName, email: `${email}@trybe.com`}
+// const aprovedEmployee = (fullName) => {
+//     const email = fullName.toLowerCase().split(' ').join('_');
+//     return { fullName, email: `${email}@trybe.com`}
+// }
+
+// const newEmployees = (callback) => {
+//     const employees = {
+//       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//       id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//     }
+//     return employees;
+//   };
+
+//   console.log(newEmployees(aprovedEmployee));
+
+const checarNumero = (meuNumero, numero) => meuNumero === numero;
+
+const resultadoSorteio = (meuNumero, callback) => {
+    const numero = Math.floor((Math.random() * 5) + 1);
+
+    return callback(meuNumero, numero) ? 'Parabéns você ganhou' : 'Tente novamente';
 }
-
-const newEmployees = (callback) => {
-    const employees = {
-      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
-
-  console.log(newEmployees(aprovedEmployee));
+console.log(resultadoSorteio(3, checarNumero));
