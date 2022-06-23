a1d7b2a07cfaad86d30e4591acaac3b10ff82db4
Rhaj Teixeira <contatorhaj@gmail.com>
2022-06-23 13:30:25 -0300

    Bonus

--- 1-fundamentos/bloco-8-higher-order-functions/script.js
+++ 1-fundamentos/bloco-8-higher-order-functions/script.js
@@ -23,24 +23,82 @@
 // }
 // console.log(resultadoSorteio(3, checarNumero));
 
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
-const compareAnswers = (rightAnswer, studentAnswer) => {
-  if (rightAnswer === studentAnswer) {
-    return 1;
-  } if (studentAnswer === 'N.A') {
-    return 0;
-  }
-  return -0.5;
-};
+// const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+// const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+// const compareAnswers = (rightAnswer, studentAnswer) => {
+//   if (rightAnswer === studentAnswer) {
+//     return 1;
+//   } if (studentAnswer === 'N.A') {
+//     return 0;
+//   }
+//   return -0.5;
+// };
 
-const countPoints = (rightAnswers, studentAnswers, action) => {
-  let contador = 0;
-  for (let index = 0; index < rightAnswers.length; index += 1) {
-    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
-    contador += actionReturn;
-  }
-  return `Resultado final: ${contador} pontos`;
-};
+// const countPoints = (rightAnswers, studentAnswers, action) => {
+//   let contador = 0;
+//   for (let index = 0; index < rightAnswers.length; index += 1) {
+//     const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+//     contador += actionReturn;
+//   }
+//   return `Resultado final: ${contador} pontos`;
+// };
 
-console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+// console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+
+const dragonAttack = (dragon) => {
+    const minDmg = 15;
+    const dragonDmg = Math.floor((Math.random() * (dragon.strength - minDmg + 1) + minDmg));
+    return dragonDmg;
+  };
+
+  const warriorAttack = (warrior) => {
+    const minDmg = warrior.strength;
+    const maxDmg = minDmg * warrior.weaponDmg;
+    const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+    return warriorDamage;
+  };
+
+  const mageAttack = (mage) => {
+    const mageMana = mage.mana;
+    const minDmg = mage.intelligence;
+    const maxDmg = minDmg * 2;
+    const turnStats = {
+      manaSpent: 0,
+      damageDealt: 'Not enough mana...',
+    };
+  
+    if (mageMana > 15) {
+      const mageDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+      turnStats.manaSpent = 15;
+      turnStats.damageDealt = mageDamage;
+      return turnStats;
+    }
+    return turnStats;
+  };
+
+  const gameActions = {
+    warriorTurn: (warriorAttack) => {
+      const warriorDamage = warriorAttack(warrior);
+      warrior.damage = warriorDamage;
+      dragon.healthPoints -= warriorDamage;
+    },
+    mageTurn: (mageAttack) => {
+        const mageTurnStats = mageAttack(mage);
+        const mageDamage = mageTurnStats.damageDealt;
+        mage.damage = mageDamage;
+        mage.mana -= mageTurnStats.manaSpent;
+        dragon.healthPoints -= mageDamage;
+      },
+      dragonTurn: (dragonAttack) => {
+        const dragonDamage = dragonAttack(dragon);
+        mage.healthPoints -= dragonDamage;
+        warrior.healthPoints -= dragonDamage;
+        dragon.damage = dragonDamage;
+      },
+      turnResults: () => battleMembers,
+    };
+  
+  gameActions.warriorTurn(warriorAttack);
+  gameActions.mageTurn(mageAttack);
+  gameActions.dragonTurn(dragonAttack);
+  console.log(gameActions.turnResults());
